6577eeebaf908890adb93d1a4c26c7593cc0f6fb
davidotalvaro94-collab <david.otalvaro94@gmail.com>
2025-09-19 19:24:43 -0500

    paso 7
    realización paso 7

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,51 @@
+import random
+from brain_games.cli import welcome_user
+
+
+def calculadora():
+    print("Welcome to the Brain Games!")
+    name = input("May I have your name? ")
+    print(f"Hello, {name}!")
+
+    operators = ["+", "-", "*"]
+    correct_answers = 0
+
+    while correct_answers < 3:
+        
+        num1 = random.randint(1, 100)
+        num2 = random.randint(1, 100)
+        operator = random.choice(operators)
+
+        expression = f"{num1} {operator} {num2}"
+
+        correct_result = eval(expression)
+
+        print("What is the result of the expression?")
+        print(f"Question: {expression}")
+        answer = input("Your answer: ")
+
+        if not answer.isdigit() and not (answer.startswith('-') and answer[1:].isdigit()):
+            print(f"'{answer}' is not a valid number. Let's try again, {name}!")
+            return
+
+        answer = int(answer)
+
+        if answer == correct_result:
+            print("Correct!")
+            correct_answers += 1
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_result}'.")
+            print(f"Let's try again, {name}!")
+            return
+
+    print(f"Congratulations, {name}!")
+
+
+def main():
+    print("Welcome to the Brain Games!")
+    name = welcome_user()
+    calculadora(name)
+
+
+if __name__ == "__main__":
+    main()
